879dc285267854550fde3695f3006fcd3ee503ad
Damon V. Caskey <dc@caskeys.com>
2020-12-21 12:16:20 -0500

    Update config.php
    Fix comment typo, default lifetime is now 1440 seconds.

--- config.php
+++ config.php
@@ -4,12 +4,12 @@ namespace dc\nahoni;
 
 abstract class DEFAULTS	
 {
-	const 	LIFE		= NULL;					// Default session lifetime.
+	const 	LIFE		= 1440;					// Default session lifetime (seconds).
 			
 			// Stored procedure default names.
-	const	SP_PREFIX	= NULL,					// Prefix for all otehr SP names.
+	const	SP_PREFIX	= NULL,					// Prefix for all other SP names.
 			SP_CLEAN	= 'session_clean',		// SP to remove all expired sessions.
-			SP_DESTROY	= 'session_destroy',	// SP to remove single session on command.
+			SP_DESTROY	= 'session_destroy',		// SP to remove single session on command.
 			SP_GET		= 'session_get',		// SP to read session data.
 			SP_SET		= 'session_set';		// SP to write session data.
 }
